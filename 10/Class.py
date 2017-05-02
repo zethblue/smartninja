cdc0486827cf007995753c00d82e327230c49b9a
Zethblue <mtauner@gmx.at>
2017-05-02 19:35:06 +0200

    02.05.17

---
+++ 10/Class.py
@@ -0,0 +1,20 @@
+
+class Snake(object):
+    name = None
+    def __init__(self, name):
+        self.name = name
+        self.belly = []
+    def eat(self, food):
+        self.belly.append(food)
+
+
+if __name__ == '__main__':
+    s1 = Snake("Billy")
+    s2 = Snake("Johnny")
+    s3 = Snake("Eddy")
+    s1.eat("Apple")
+    s1.eat("Honey")
+    s2.eat("Spaghetti")
+
+    print s1.belly
+    print s1.__dict__
